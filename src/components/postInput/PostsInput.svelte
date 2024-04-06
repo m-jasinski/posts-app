eec1e5556f0c8b8ca1f59108936b34102af96d6f
Marek Jasinski <codigo@codigo.pl>
2024-04-06 19:10:28 +0200

    adjust input range

--- src/components/postInput/PostsInput.svelte
+++ src/components/postInput/PostsInput.svelte
@@ -5,9 +5,18 @@
 	let postVal: number;
 	const minVal = 1;
 	const maxVal = 20;
+	let inputEl: HTMLInputElement;
+
 	onMount(() => {
 		postVal = howManyPosts;
+		inputEl.style.setProperty('--value', '' + postVal);
 	});
+
+	$: {
+		if (inputEl) {
+			inputEl.style.setProperty('--value', '' + postVal);
+		}
+	}
 
 	function updateData(value: Event): void {
 		howManyPosts = +(value.target as HTMLInputElement).value;
@@ -18,13 +27,14 @@
 	<fieldset class="flex flex-col">
 		<label for="post-length" class="mb-2">Set number of posts to render (1-20)</label>
 		<input
+			bind:this={inputEl}
 			id="post-length"
 			type="range"
 			bind:value={postVal}
 			min="1"
 			max="20"
 			on:change={(value) => updateData(value)}
-			class="bg-white"
+			class="styled-slider slider-progress my-2"
 		/>
 		<p>{postVal} {postVal > 1 ? 'posts' : 'post'}</p>
 	</fieldset>
